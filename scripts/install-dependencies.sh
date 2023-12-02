b84ce0af0dcf18796cc7ce02697d8be3ae8ff06b
YU88John <mtk.john88@gmail.com>
2023-12-02 10:46:50 +0400

    install npm

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 set -e
 
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash -
+yum install nodejs -y
+
+# Wait for npm to be available (wait for 10 seconds)
+echo "Waiting for npm to be available..."
+sleep 10
+
 echo "Installing global dependencies..."
 npm install -g pm2
+pm2 update
 
-echo "Installing application dependencies..."
-cd /var/app/current
-npm install
+# delete existing bundle
+cd /home/ec2-user
+rm -rf node
+
+echo "Script completed successfully."
